90c2d00f61f1d60229dcd90cc45b7b7c866b4951
basir <m-basir@bk.ru>
2025-07-09 13:08:20 +0300

    feat: create user model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from users.models import User
+
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'status')
+    search_fields = ('email', 'first_name', 'last_name')
+    list_filter = ('status',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'phone', 'address')}),
+        ('Permissions', {'fields': ('status', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'first_name', 'last_name', 'phone', 'address', 'status',
+                       'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+    )
+
+    ordering = ('email',)
+
+admin.site.register(User, CustomUserAdmin)
+
--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    STATUS_CHOICES = [
+        ("active", "Active"),
+        ("blocked", "Blocked"),
+    ]
+
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20)
+    address = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="active")
+
+    username = None
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
